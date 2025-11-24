fbc8ca63b77dfb075fa7704624284561853510de
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 14:26:38 +0700

    feat(layout): Home Tab Navigation - Recomendation page - liked opponents - explore - chat - profile

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,85 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { Fontisto, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	// const colorScheme = useColorScheme() || "light";
+	const colorScheme = "light";
+	const iconSize = 24;
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarStyle: {
+					backgroundColor: Colors[colorScheme].background,
+					borderTopWidth: 0,
+					height: 60,
+				},
+				tabBarItemStyle: {
+					paddingVertical: Platform.OS === "android" ? 4 : 10,
+				},
+				tabBarLabelStyle: {
+					marginBottom: 0,
+				},
+				tabBarActiveTintColor: Colors[colorScheme].tint,
+				headerShown: false,
+				tabBarShowLabel: false,
+				tabBarButton: HapticTab,
+			}}
+		>
+			<Tabs.Screen
+				name="recommended"
+				options={{
+					title: "Recommendations",
+					tabBarIcon: ({ color, focused }) => (
+						<Fontisto
+							size={iconSize}
+							name="tinder"
+							color={focused ? "#FD3A73" : color}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="likedOpponents"
+				options={{
+					title: "Liked Opponents",
+					tabBarIcon: ({ color }) => (
+						<MaterialCommunityIcons
+							size={iconSize}
+							name="star-four-points"
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="explore"
+				options={{
+					title: "Explore",
+					tabBarIcon: ({ color }) => (
+						<Ionicons size={iconSize} name="grid" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="chat"
+				options={{
+					tabBarIcon: ({ color }) => (
+						<Ionicons size={iconSize} name="chatbubbles-sharp" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="profile"
+				options={{
+					tabBarIcon: ({ color }) => (
+						<Ionicons size={iconSize} name="person" color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
 }
