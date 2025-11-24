1f37787b3ba9bdfef9f159de32586309b1d86a95
Ajhoel Tragedy <panduwiranata43@gmail.com>
2025-11-24 16:32:24 +0700

    feat(page): Recommended People  - modify styling

--- components/ui/people-card.tsx
+++ components/ui/people-card.tsx
@@ -1,37 +1,28 @@
 import type { AccountProps } from "@/types/account";
 import { Feather } from "@expo/vector-icons";
 import { memo } from "react";
-import { Dimensions, Image, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 type PeopleCardProps = {
 	data: AccountProps;
 };
-const screenWidth = Dimensions.get("window").width;
-const ASPECT_RATIO = 3 / 4;
-const dynamicHeight = screenWidth / ASPECT_RATIO;
+// const screenWidth = Dimensions.get("window").width;
+// const ASPECT_RATIO = 3 / 4;
+// const dynamicHeight = screenWidth / ASPECT_RATIO;
 
 const PeopleCard = memo(function PeopleCard({ data }: PeopleCardProps) {
-
 	return (
-		<View
-			className="relative overflow-hidden shadow-lg bg-amber-400 mx-4"
-			style={{
-				width: screenWidth,
-				height: dynamicHeight,
-			}}
-		>
+		<View className="relative overflow-hidden shadow-lg h-[80%] rounded-3xl mx-2">
 			<Image
 				source={{ uri: data.pictures }}
-				style={{ width: "100%", height: "100%" }}
 				className="w-full h-full absolute top-0 left-0"
 				resizeMode="cover"
 			/>
-			{/* <View className="absolute top-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-black/60 to-transparent"> */}
-			<View className="absolute top-0 left-0 right-0 p-4 bg-amber-400">
-				<Text className="text-white text-lg font-bold bg-cyan-500">
+			<View className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-black/40 to-transparent h-28 gap-2">
+				<Text className="text-white text-lg font-bold">
 					{data.name}, {data.age}
 				</Text>
-				<Text className="text-white">
+				<Text className="text-white flex gap-2 items-center">
 					<Feather name="map-pin" size={18} />
 					{data.location}
 				</Text>
